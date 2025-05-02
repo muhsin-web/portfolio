6da663932f245331e639c3cda936e04536ba1267
MUHSIN | App Dev <85878363+muhsin-web@users.noreply.github.com>
2025-05-02 08:56:36 +0100

    seo data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "John Doe - Developer",
+  title: "Muhsin - Developer",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Muhsin Abdulrasheed is a software mobile and web developer, also a designer working remotely.",
 };
 
 export default function RootLayout({
